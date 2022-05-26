f6ff4595b29edb689e68bbb0ae847d03367b2d84
noothanks <coreyfuller727@yahoo.com>
2022-05-25 21:52:52 -0400

    adding initial calendar

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,25 @@
 var signInButton = document.querySelector("#sign-in-button");
 var formDisplay = document.querySelector("#sign-in-form");
-signInButton.addEventListener("click", function() {
-    formDisplay.classList.add('hidden');
-})
+var introP = document.querySelector("#intro-paragraph");
+var calendarEl = document.getElementById('calendar');
+signInButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    introP.innerHTML = "";
+    introP.classList.add('is-invisible');
+    
+
+    formDisplay.innerHTML = "";
+    formDisplay.classList.add('is-invisible');
+    
+    console.log("doug");
+    calendarEl.classList.remove('is-invisible');
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
+    });
+
+    calendar.render(); 
+  });
