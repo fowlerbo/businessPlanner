c8af3448e7fff785b0961281ba11ae8c898f8e02
noothanks <coreyfuller727@yahoo.com>
2022-05-30 16:35:55 -0400

    Adding calendar functionality and editing display

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,28 +1,22 @@
 var signInButton = document.querySelector("#sign-in-button");
 var formDisplay = document.querySelector("#sign-in-form");
-var introP = document.querySelector("#intro-paragraph");
+var welcomeWrapper = document.querySelector(".welcome-wrapper");
 var calendarEl = document.getElementById('calendar');
+var events = []
+//login to local storage
+//if login is true then dont show sign in
 
-signInButton.addEventListener("click", function(event) {
-    event.preventDefault();
 
-    introP.innerHTML = "";
-    introP.classList.add('is-invisible');
-    
-
-    formDisplay.innerHTML = "";
-    formDisplay.classList.add('is-invisible');
-    
-    console.log("doug");
-    calendarEl.classList.remove('is-invisible');
-});
 
 document.addEventListener('DOMContentLoaded', function() {
     var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'timeGridWeek',
+      initialView: 'dayGridMonth',
       selectable: true,
       navLinks: true,
       nowIndicator: true,
+      height: "100vh",
+      //width: "100%",
+      //expandRows: true,
       buttonText: {
         addEvent: 'Add Event'
       },
@@ -43,28 +37,54 @@ document.addEventListener('DOMContentLoaded', function() {
         addEventButton: {
           text: 'Add Event',
           click: function() {
-            var userTitle = prompt('Give your event a name.')
-            var dateSelect = prompt('Enter a date in YYYY-MM-DD format');            
+            var userTitle = prompt('Give your event a name.');
+            var dateSelect= prompt('Enter a date in YYYY-MM-DD format');
             var timeSelect = prompt('Enter a time in T00:00:00 format (ex. T14:00:00 will render 2:00 PM');
             var date = new Date(dateSelect + timeSelect);
-//get date and time
-//select if event is all day or not
-            //if (!isNaN(date.valueOf())) {
-              calendar.addEvent({
-                title: userTitle,
-                start: date,
-                eventDisplay: 'auto'
-              });
-            //} else {
-              //alert('Invalid information.')
-            //}
+            var eventId = 0
+            var eventInfo = {
+              title: userTitle,
+              start: date,        
+              eventDisplay: 'auto',
+              id: eventId
+            }
+            var addEventFunction = function() {
+              calendar.addEvent(eventInfo);
+              eventId++
+              }
+            
+            events.push(eventInfo);
+            console.log(events)
+            
+            addEventFunction();
+            localStorage.setItem("Event List", JSON.stringify(events));
           }
         }
       }
     });
+    
 
+    events = JSON.parse(localStorage.getItem("Event List"));
+    for (i = 0; i<events.length; i++) {
+      calendar.addEvent(events[i]);
+    }
+    calendar.updateSize();
     calendar.render();
+    
+    signInButton.addEventListener("click", function(event) {
+      event.preventDefault();
+  
+      welcomeWrapper.innerHTML = "";
+      welcomeWrapper.classList.add('is-invisible');
       
+  
+      formDisplay.innerHTML = "";
+      formDisplay.classList.add('is-invisible');
+      
+      calendarEl.classList.remove('is-invisible');
+      calendar.render();
+  });
+    
   });
 
 
