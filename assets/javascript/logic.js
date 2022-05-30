d2aaf6cc6c3e60866d6cac5fc92dd67dd521ef6d
noothanks <coreyfuller727@yahoo.com>
2022-05-30 14:33:49 -0400

    updating add event button to include a specific time

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,6 +2,7 @@ var signInButton = document.querySelector("#sign-in-button");
 var formDisplay = document.querySelector("#sign-in-form");
 var introP = document.querySelector("#intro-paragraph");
 var calendarEl = document.getElementById('calendar');
+
 signInButton.addEventListener("click", function(event) {
     event.preventDefault();
 
@@ -26,9 +27,9 @@ document.addEventListener('DOMContentLoaded', function() {
         addEvent: 'Add Event'
       },
       headerToolbar:  {
-          start: 'title',
-          center: 'dayGridMonth,timeGridWeek today prev,next',
-          end: 'prevYear,nextYear'
+          start: 'prev,next today',
+          center: 'title',
+          end: 'dayGridMonth,timeGridWeek,timeGridDay'
         },
       businessHours: {
         daysOfWeek: [1, 2, 3, 4, 5],
@@ -43,18 +44,20 @@ document.addEventListener('DOMContentLoaded', function() {
           text: 'Add Event',
           click: function() {
             var userTitle = prompt('Give your event a name.')
-            var dateStr = prompt('Enter a date in YYYY-MM-DD format');
-            var date = new Date(dateStr + 'T00:00:00');
-
-            if (!isNaN(date.valueOf())) {
+            var dateSelect = prompt('Enter a date in YYYY-MM-DD format');            
+            var timeSelect = prompt('Enter a time in T00:00:00 format (ex. T14:00:00 will render 2:00 PM');
+            var date = new Date(dateSelect + timeSelect);
+//get date and time
+//select if event is all day or not
+            //if (!isNaN(date.valueOf())) {
               calendar.addEvent({
                 title: userTitle,
                 start: date,
                 eventDisplay: 'auto'
               });
-            } else {
-              alert('Invalid information.')
-            }
+            //} else {
+              //alert('Invalid information.')
+            //}
           }
         }
       }
