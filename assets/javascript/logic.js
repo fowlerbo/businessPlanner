c1251e64b357d54d31ae4a39675ec71e4e686dd5
Anillag <101154249+anillag@users.noreply.github.com>
2022-05-30 23:09:04 -0400

    Stock prices proof-of-concept

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,8 +2,10 @@ var signInButton = document.querySelector("#sign-in-button");
 var formDisplay = document.querySelector("#sign-in-form");
 var welcomeWrapper = document.querySelector(".welcome-wrapper");
 var calendarEl = document.getElementById('calendar');
+var stocksEl = document.querySelector("#stock-prices");
 var events = []
 var modal = document.querySelector(".modal")
+var stockKey = "API_KEY92DYBC7BL2AM3H5A8BJ2IM4QVAEPY456";
 //login to local storage
 //if login is true then dont show sign in
 
@@ -126,12 +128,47 @@ document.addEventListener('DOMContentLoaded', function() {
       formDisplay.classList.add('is-invisible');
       
       calendarEl.classList.remove('is-invisible');
+      stocksEl.classList.remove('is-invisible');
       calendar.render();
-    
+      stockPrices();
     
 
     });
     
   });
 
-
+// Stock Ticker
+var stockPrices = function() {
+  var apiUrl1 = "https://api.finage.co.uk/last/stock/AAPL?apikey=" + stockKey;
+  var apiUrl2 = "https://api.finage.co.uk/last/stock/TSLA?apikey=" + stockKey;
+  var apiUrl3 = "https://api.finage.co.uk/last/stock/GOGL?apikey=" + stockKey;
+  console.log("apiUrl1: " + apiUrl1);
+  console.log("apiUrl2: " + apiUrl2);
+  console.log("apiUrl3: " + apiUrl3);
+  fetch(apiUrl1)
+    .then(function(response) {
+      if (response.ok) {
+        response.json()
+          .then(function(stockData1) {
+            var testing = JSON.stringify(stockData1.symbol);
+            console.log("testing:  " + testing);
+            // var date = new Date(JSON.stringify(data.current.dt)*1000).toLocaleDateString("en-US");
+            // var icon = JSON.stringify(data.current.weather[0].icon).slice(1, -1);
+            // cityDateEl.innerHTML = city + " " + date +
+            // "<img id='city-state-image' src='https://openweathermap.org/img/wn/" + icon + ".png'>";
+            // var temp = parseFloat((JSON.stringify(data.current.temp)-273.15)*1.8+32).toFixed(0);
+            // tempEl.textContent = "Temp: " + temp + "°F";
+            // var wind = parseFloat(JSON.stringify(data.current.wind_speed)).toFixed(0);
+            // windEl.textContent = "Wind: " + wind + " mph";
+            // var humidity = JSON.stringify(data.current.humidity);
+            // humidityEl.textContent = "Humidity: " + humidity + "%";
+            // var uvIndex = JSON.stringify(data.current.uvi);
+            // uvIndexEl.textContent = "UV Index: " + uvIndex;
+          });
+      } else {
+          console.log("stockPrices error response " + response.statusText);
+      };
+    });
+};
+  
+stockPrices();
