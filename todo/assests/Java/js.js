1ae92e1bc9bc75538fcc959db321c31c151864b8
Andrew Freeman <andrew.p.freeman@outlook.com>
2022-06-01 18:40:17 -0400

    update

--- todo/assests/Java/js.js
+++ todo/assests/Java/js.js
@@ -1,50 +1,59 @@
-var myList = document.getElementsByTagName("li");
-var i;
-for (i=0; i < myList.length; i++) {
-    var span= document.createElement("span");
-    var text = document.createTextNode("/u00D7");
-    span.className= "close";
-    span.appendChild(txt);
-    myList [I]. appendChild(span);
-}
-var close = document.getElementsByClassName("close");
-var i;
-for (i=0; i < close.length; i++) {
-    close[i].onclick= function () {
-        var div=this.parentelement;
-        div.style.display = "none";
+const input = document.getElementsByClassName('input')[0];
+
+ const addSubmitBtn = document.getElementsByClassName('submit')[0];
+ 
+
+addSubmitBtn.addEventListener('click', function (){
+  
+if(input.value.trim()!=0){
+       let localItems = JSON.parse( localStorage.getItem('localItem'))
+    if(localItems === null){
+         taskList = []
+
+    }else{
+        taskList = localItems;
     }
+    taskList.push(input.value)
+    localStorage.setItem('localItem', JSON.stringify(taskList)); 
 }
 
-var list = document.querySelector ('ul');
-list.addEventListener('click', function (ev) {
-    if (ev.target.getElementsByTagName === 'LI'){
-        ev.target.classList.toggle('checked');
-    }
-}, false);
+    displayItem()
+})
 
-function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-      alert("You must write something!");
-    } else {
-      document.getElementById("myUL").appendChild(li);
+function displayItem(){
+    let localItems = JSON.parse( localStorage.getItem('localItem'))
+    if(localItems === null){
+         taskList = []
+
+    }else{
+        taskList = localItems;
     }
-    document.getElementById("myInput").value = "";
-  
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-  
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-      }
-    }
-  } 
+
+let html = '';
+let itemShow = document.querySelector('.tasks');
+taskList.forEach((data, index )=> {
+    
+
+    html += `
+    <div class="tasks">
+    <p class="pText">${data}</p>
+    <button class="deleteTask" onClick="deleteItem(${index})">x</button>
+    </div>
+    `
+})
+itemShow.innerHTML = html;
+}
+displayItem()
+
+function deleteItem(index){
+    let localItems = JSON.parse( localStorage.getItem('localItem'))
+    taskList.splice(index, 1)
+    localStorage.setItem('localItem', JSON.stringify(taskList));
+    displayItem()
+}
+
+function clearTask(){
+    
+localStorage.clear()
+displayItem()
+}
